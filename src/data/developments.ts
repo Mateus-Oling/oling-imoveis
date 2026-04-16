cb5e8bbac9c570fa56f7074bcf28d65208ed0d67
Mateus Oling <mateus.oling@hotmail.com>
2026-04-16 14:43:09 -0300

    feat: add mock data for developments

---
+++ src/data/developments.ts
@@ -0,0 +1,20 @@
+export const developments = [
+  {
+    id: 223,
+    image: "/capa03.png",
+    type: "Apartamento",
+    title: "Tríade Towers",
+    neighborhood: "bairro Universitário",
+    address: "Rua dos Universitários, 544",
+    area: 350,
+  },
+  {
+    id: 224,
+    image: "/capa04.jpg",
+    type: "Terreno",
+    title: "Colina",
+    neighborhood: "bairro Glória",
+    address: "Rua dos Glorianos, 254",
+    area: 300,
+  },
+]
